d2982e8202b947dac05376c952042e4b72887ad0
hummingengineer <hummingengineer@gmail.com>
2020-04-29 14:56:43 +0900

    Feat: Add vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,23 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  "pluginOptions": {
+    "electronBuilder": {
+      "builderOptions": {
+        "win": {
+          "target": [
+            {
+              "target": "portable",
+              "arch": "x64"
+            }
+          ],
+          // "icon": require('path').join('public', '')
+        },
+        "portable": {
+          "artifactName": "image-file-manager_portable_win_x64.exe"
+        }
+      }
+    }
+  }
 }
